aaebe0229ec76e427c395b7f18a96a775b0bac8e
punkystone <email@punkystone.de>
2025-07-24 17:34:13 +0200

    add okaygarmin

--- cmd/punkygpt/main.go
+++ cmd/punkygpt/main.go
@@ -11,6 +11,7 @@ import (
 )
 
 var chatRegex = regexp.MustCompile(`^((?i)@punkygpt),?\s(.+)`)
+var okayGarminRegex = regexp.MustCompile(`^OKAYGARMIN (erstelle clip|setzte (spiel|titel) auf (.*))`)
 
 func main() {
 	env, err := internal.CheckEnv()
@@ -26,11 +27,25 @@ func main() {
 	}()
 
 	client.IRCClient.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		trimmedMessage := strings.TrimSpace(message.Message)
+		matches := okayGarminRegex.FindAllStringSubmatch(trimmedMessage, -1)
+		if len(matches) == 1 {
+			match := matches[0]
+			if match[2] == "" && match[3] == "" {
+				client.IRCClient.Say(message.Channel, "=clip")
+			} else {
+				switch match[2] {
+				case "spiel":
+					client.IRCClient.Say(message.Channel, "!setgame "+match[3])
+				case "titel":
+					client.IRCClient.Say(message.Channel, "!settitle "+match[3])
+				}
+			}
+		}
 		if message.Reply != nil {
 			return
 		}
-		trimmedMessage := strings.TrimSpace(message.Message)
-		matches := chatRegex.FindAllStringSubmatch(trimmedMessage, -1)
+		matches = chatRegex.FindAllStringSubmatch(trimmedMessage, -1)
 		if len(matches) == 0 || len(matches[0]) != 3 {
 			return
 		}
